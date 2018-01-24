ae58555fbfe81cf8d18948498fad7d80aa577267
Matti Saarela <mattis@matti-macbook-pro.local>
2018-01-24 13:36:04 +0200

    Initial Commit

---
+++ ankka_new/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ankka_new
+//
+//  Created by Matti Saarela on 24/01/2018.
+//  Copyright © 2018 Matti Saarela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
